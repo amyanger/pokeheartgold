afbe98e38b04df69fea2d5e209a048938ccc6edd
arjun-myanger <admyanger@outlook.com>
2026-04-12 02:11:28 +0100

    Decompile unk_02078DD8, unk_020551B8, unk_02097B78
    - unk_02078DD8: trainer profile data builder for battle system (2 functions)
    - unk_020551B8: field effect task with BGM transition state machine (2 functions)
    - unk_02097B78: legendary cinematic overlay - Ho-Oh/Lugia/Arceus cutscenes (3 functions)

---
+++ src/unk_020551B8.c
@@ -0,0 +1,49 @@
+#include "unk_020551B8.h"
+
+#include "global.h"
+
+#include "heap.h"
+#include "sound_02004A44.h"
+#include "task.h"
+
+extern void ov01_021EFB64(int effect);
+extern void ov01_021EFC94(int effect, FieldSystem *fieldSystem, int *result);
+extern void ov01_021EFC04(int effect);
+
+typedef struct {
+    int state;
+    int result;
+    int effect;
+    u16 bgm;
+} UnkStruct_020551B8;
+
+static BOOL sub_020551B8(TaskManager *man) {
+    FieldSystem *fieldSystem = TaskManager_GetFieldSystem(man);
+    UnkStruct_020551B8 *env = TaskManager_GetEnvironment(man);
+
+    switch (env->state) {
+    case 0:
+        ov01_021EFB64(env->effect);
+        ov01_021EFC94(env->effect, fieldSystem, &env->result);
+        Sound_SetSceneAndPlayBGM(5, (u16)env->bgm, 1);
+        env->state++;
+        break;
+    case 1:
+        if (env->result == 1) {
+            ov01_021EFC04(env->effect);
+            Heap_Free(env);
+            return TRUE;
+        }
+        break;
+    }
+    return FALSE;
+}
+
+void sub_02055218(TaskManager *man, int effect, int bgm) {
+    UnkStruct_020551B8 *env = Heap_AllocAtEnd(HEAP_ID_FIELD2, sizeof(UnkStruct_020551B8));
+    env->state = 0;
+    env->result = 0;
+    env->effect = effect;
+    env->bgm = bgm;
+    TaskManager_Call(man, (TaskFunc)sub_020551B8, env);
+}
---
+++ src/unk_02097B78.c
@@ -0,0 +1,48 @@
+#include "unk_02097B78.h"
+
+#include <nitro/mi/memory.h>
+
+#include "global.h"
+
+#include "heap.h"
+#include "constants/heap.h"
+#include "overlay_manager.h"
+
+typedef BOOL (*LegendaryCinematicFunc)(LegendaryCinematicArgs *args);
+
+extern BOOL ScriptCinematic_HoOh(LegendaryCinematicArgs *args);
+extern BOOL ScriptCinematic_Lugia(LegendaryCinematicArgs *args);
+extern BOOL ScriptCinematic_Arceus(LegendaryCinematicArgs *args);
+
+static const LegendaryCinematicFunc sLegendaryCinematicFuncs[] = {
+    ScriptCinematic_HoOh,
+    ScriptCinematic_Lugia,
+    ScriptCinematic_Arceus,
+};
+
+typedef struct LegendaryCinematicData {
+    LegendaryCinematicArgs *args;
+    u8 unk[0x418];
+} LegendaryCinematicData;
+
+BOOL LegendaryCinematic_Init(OverlayManager *man, int *state) {
+    Heap_Create(HEAP_ID_3, HEAP_ID_153, 2 << 18);
+    LegendaryCinematicData *data = OverlayManager_CreateAndGetData(man, 0x41C, HEAP_ID_153);
+    MI_CpuFill8(data, 0, 0x41C);
+    data->args = OverlayManager_GetArgs(man);
+    return TRUE;
+}
+
+BOOL LegendaryCinematic_Main(OverlayManager *man, int *state) {
+    LegendaryCinematicData *data = OverlayManager_GetData(man);
+    if (sLegendaryCinematicFuncs[data->args->unk4C](data->args) == 0) {
+        return TRUE;
+    }
+    return FALSE;
+}
+
+BOOL LegendaryCinematic_Exit(OverlayManager *man, int *state) {
+    OverlayManager_FreeData(man);
+    Heap_Destroy(HEAP_ID_153);
+    return TRUE;
+}
